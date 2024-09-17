ac9ff856b2e900257a13271981b3fa8eeb4cb508
haripajjuri <20mh1a4241@acoe.edu.in>
2024-09-17 19:01:39 +0530

    jasdf

--- app/api/sendMessage/route.js
+++ app/api/sendMessage/route.js
@@ -4,10 +4,8 @@ export async function POST(req) {
     const { msg, roomId } = await req.json();
     console.log("msg :" + msg);
     console.log("roomId :" + roomId);
-    try {
-        pusherServer.trigger(roomId, "incoming-message", msg);
-    } catch (err) {
-        return new Response(JSON.stringify(err));
-    }
+
+    await pusherServer.trigger(roomId, "incoming-message", msg);
+
     return new Response(JSON.stringify({ success: "true" }));
 }
